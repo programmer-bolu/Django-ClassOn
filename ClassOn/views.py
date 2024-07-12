f07f312a554f1f5b56d29759f1e40539e957363b
Akingbade Boluwatife Ilerioluwa <akingabdeboluwatife030@gmail.com>
2024-07-12 14:59:39 +0100

    eere

--- ClassOn/views.py
+++ ClassOn/views.py
@@ -3,7 +3,20 @@ from .models import schedule
 import datetime
 # Create your views here.
 def index(request):
-    time = datetime.datetime.now()
-    time = time.strftime("%B-%d-%Y")
+    date = datetime.datetime.now()
+    date = date.strftime("%m-%d-%Y")
     schedules = schedule.objects.all()
-    return render(request, 'pages/index.html', {'schedules': schedules, 'date': time})
+    
+    for items in schedules:
+        items.class_date = items.class_date.strftime("%m-%d-%Y")
+        print(items.class_date)
+
+        # if schedules < time:
+        #     items.class_date = False
+        # elif schedules > time:
+        #     True
+        # else:
+        #     'data'
+
+    
+    return render(request, 'pages/index.html', {'schedules': schedules, 'date': date})
