4608974bc25918319ccb009d7637ec70fc1ed0e5
Akingbade Boluwatife Ilerioluwa <akingabdeboluwatife030@gmail.com>
2024-07-12 16:01:27 +0100

    updated

--- ClassOn/views.py
+++ ClassOn/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
 from .models import schedule
-import datetime
 # Create your views here.
 def index(request):
-    date = datetime.datetime.now()
-    date = date.strftime("%m-%d-%Y")
     schedules = schedule.objects.all()
-    
-    for items in schedules:
-        items.class_date = items.class_date.strftime("%m-%d-%Y")
-        print(items.class_date)
-
-        # if schedules < time:
-        #     items.class_date = False
-        # elif schedules > time:
-        #     True
-        # else:
-        #     'data'
+    format_schedule = schedules[::-1]
 
     
-    return render(request, 'pages/index.html', {'schedules': schedules, 'date': date})
+    return render(request, 'pages/index.html', {'schedules': format_schedule})
+
