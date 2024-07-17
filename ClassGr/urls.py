c0ee746f41f26ce7d6967cf21fb8afa1ae3590da
Akingbade Boluwatife Ilerioluwa <akingabdeboluwatife030@gmail.com>
2024-07-17 02:18:41 +0100

    up

--- ClassGr/urls.py
+++ ClassGr/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='static-pages/home.html'), name='home'),
     path('signup/', TemplateView.as_view(template_name='static-pages/createanaccount.html'), name='createaccount'),
+    path('about/', TemplateView.as_view(template_name='static-pages/about.html'), name='about'),
 ]
