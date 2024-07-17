bdaf16da97190fdcf76ad650ee1eeaab0d6f9c94
Akingbade Boluwatife Ilerioluwa <akingabdeboluwatife030@gmail.com>
2024-07-17 10:09:20 +0100

    ddd

--- ClassGr/urls.py
+++ ClassGr/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='static-pages/home.html'), name='home'),
     path('signup/', TemplateView.as_view(template_name='static-pages/createanaccount.html'), name='createaccount'),
     path('about/', TemplateView.as_view(template_name='static-pages/about.html'), name='about'),
+    path('feedback/', TemplateView.as_view(template_name='static-pages/feedback.html'), name='feedback'),
 ]
