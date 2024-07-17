1e421cf1fb9e0088d70ad27044be969abb024a53
Akingbade Boluwatife Ilerioluwa <akingabdeboluwatife030@gmail.com>
2024-07-17 15:32:29 +0100

    update

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ClassGr.urls')),
-    path('', include('signup.urls'))
+    path('', include('signup.urls')),
+    path('', include('login.urls'))
 ]
